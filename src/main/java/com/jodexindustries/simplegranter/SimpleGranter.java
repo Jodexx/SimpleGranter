d0051633ad2666dbf8a5a302a51f0026047b74e0
JodexIndustries <0984134q@gmail.com>
2023-06-14 14:50:05 +0300

    1.0.0

---
+++ src/main/java/com/jodexindustries/simplegranter/SimpleGranter.java
@@ -0,0 +1,17 @@
+package com.jodexindustries.simplegranter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleGranter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
