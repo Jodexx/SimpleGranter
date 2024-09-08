15be2fadbfe0bb83951b85e32d7be0298cf4141c
Jodexx <0984134q@gmail.com>
2024-09-08 15:26:22 +0300

    Fix issue with luckperms driver

--- src/main/java/com/jodexindustries/simplegranter/api/GranterAPI.java
+++ src/main/java/com/jodexindustries/simplegranter/api/GranterAPI.java
@@ -2,29 +2,44 @@ package com.jodexindustries.simplegranter.api;
 
 import com.jodexindustries.simplegranter.fields.PermissionDriver;
 import net.luckperms.api.model.group.Group;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 import static com.jodexindustries.simplegranter.SimpleGranter.*;
 
 public class GranterAPI {
+
+    public String getGroupPrefix(String group) {
+        String prefix = null;
+        if (permissionDriver == PermissionDriver.vault) {
+            prefix = chat.getGroupPrefix(Bukkit.getWorlds().get(0), group);
+        } else {
+            Group lpGroup = luckPerms.getGroupManager().getGroup(group);
+            if (lpGroup != null) prefix = lpGroup.getCachedData().getMetaData().getPrefix();
+        }
+
+        return prefix;
+    }
+
     /**
      * Get player group
+     *
      * @param player Bukkit Player
      * @return Group name
      */
     public String getPlayerGroup(Player player) {
         String group = "";
-        if(permissionDriver == PermissionDriver.vault) group = perms.getPrimaryGroup(player);
-        if(permissionDriver == PermissionDriver.luckperms) group = luckPerms.getPlayerAdapter(Player.class).getUser(player).getPrimaryGroup();
+        if (permissionDriver == PermissionDriver.vault) group = perms.getPrimaryGroup(player);
+        if (permissionDriver == PermissionDriver.luckperms)
+            group = luckPerms.getPlayerAdapter(Player.class).getUser(player).getPrimaryGroup();
         return group;
     }
+
     /**
      * Check if the player can give groups.
+     *
      * @param player A player who gives
      * @return Boolean
      */
@@ -35,18 +50,26 @@ public class GranterAPI {
 
     /**
      * Check if the group exists
+     *
      * @param group Group name
      * @return Boolean
      */
     public boolean isGroupExist(String group) {
-        List<String> groupsList = new ArrayList<>();
-        if(permissionDriver == PermissionDriver.vault) Collections.addAll(groupsList, perms.getGroups());
-        if(permissionDriver == PermissionDriver.luckperms) luckPerms.getGroupManager().getLoadedGroups().stream().map(Group::getName).collect(Collectors.toList());
-        return groupsList.contains(group);
+        switch (permissionDriver) {
+            case vault:
+                return Arrays.asList(perms.getGroups()).contains(group);
+            case luckperms:
+                return luckPerms.getGroupManager().getLoadedGroups()
+                        .stream()
+                        .map(Group::getName)
+                        .anyMatch(group::equals);
+        }
+        return false;
     }
 
     /**
      * Get group level
+     *
      * @param group Group name
      * @return Group level
      */
@@ -56,22 +79,24 @@ public class GranterAPI {
 
     /**
      * Get the number of groups a group can give.
+     *
      * @param senderGroup The group of the giving player
-     * @param group The group the player wants to give
+     * @param group       The group the player wants to give
      * @return Number of groups
      */
     public int getGroupInConfig(String senderGroup, String group) {
-        return yaml.getConfig().getInt("Settings.Groups." + senderGroup + "." + group,0);
+        return yaml.getConfig().getInt("Settings.Groups." + senderGroup + "." + group, 0);
     }
 
     /**
      * Get the number of groups that the player can still give.
+     *
      * @param player A player who gives
-     * @param group The group the player wants to give
+     * @param group  The group the player wants to give
      * @return Number of group usage
      */
     public int getGroupInData(Player player, String group) {
-        if(!sql) {
+        if (!sql) {
             return yaml.getData().getInt("Players." + player.getName() + "." + group, 0);
         } else {
             return mysql.getCount(player.getName(), group);
@@ -80,12 +105,13 @@ public class GranterAPI {
 
     /**
      * Set group usages for player
+     *
      * @param player Player, who used grant
-     * @param group The group the player wants to give
-     * @param count Number of group usage
+     * @param group  The group the player wants to give
+     * @param count  Number of group usage
      */
     public void setGroupUsages(Player player, String group, int count) {
-        if(!sql) {
+        if (!sql) {
             yaml.getData().set("Players." + player.getName() + "." + group, count);
             yaml.saveData();
         } else {
